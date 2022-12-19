e803da38e91ecb6d6803176fc8f1688c5386d360
Armen <madarmen1987@gmail.com>
2022-12-19 18:45:58 +0400

    Work with UI

---
+++ GuessTheNumber/ButtonView.swift
@@ -0,0 +1,25 @@
+//
+//  Button.swift
+//  GuessTheNumber
+//
+//  Created by Armen Madoyan on 19.12.2022.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    let label: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(label, action: action)
+            .padding()
+        
+    }
+}
+
+struct Button_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonView(label: "Button", action: {} )
+    }
+}
--- GuessTheNumber/ContentView.swift
+++ GuessTheNumber/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number = 10
+    @State private var value = 0.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("Move the slider closer to: ")
+                    .padding()
+                Text("\(number)")
+                
+            }
+
+            Slider(value: $value, in: 0...100, step: 1)
+            ButtonView(label: "Check me!", action: {})
+            ButtonView(label: "New Game!", action: {})
+            
         }
         .padding()
     }
